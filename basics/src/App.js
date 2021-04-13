15d235ae026d10cd6752080eddf3329fa0e79c58
michael rondon <mrondon72@gmail.com>
2021-04-13 20:46:36 +0200

    38. Splitting Components Into Multiple Components

--- basics/src/App.js
+++ basics/src/App.js
@@ -36,25 +36,25 @@ function App() {
         amount={ expenses[0].amount }
         date={ expenses[0].date }
         title={ expenses[0].title }
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         amount={ expenses[1].amount }
         date={ expenses[1].date }
         title={ expenses[1].title }
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         amount={ expenses[2].amount }
         date={ expenses[2].date }
         title={ expenses[2].title }
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         amount={ expenses[3].amount }
         date={ expenses[3].date }
         title={ expenses[3].title }
-      ></ExpenseItem>
+      />
     </div>
   );
 }
--- basics/src/components/ExpenseItem.js
+++ basics/src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
+// CSS
 import './ExpenseItem.css';
 
+// JSX
+import ExpenseDate from './ExpenseDate';
+
 function ExpenseItem( props ) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = props.date.getFullYear();
+    
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{ month }</div>
-                <div>{ year }</div>
-                <div>{ day }</div>
-            </div>
+            <ExpenseDate date={ props.date }/>
 
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
