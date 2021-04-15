aa85381f30d02e456b225dec35626ff04ab1ebca
michael rondon <mrondon72@gmail.com>
2021-04-16 01:38:48 +0200

    60. Controlled vs Uncontrolled Components & Stateless vs Statefull Components

--- basics/src/components/Expenses/ExpenseItem.js
+++ basics/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // CSS
 import './ExpenseItem.css';
@@ -8,27 +8,15 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem( props ) {
-    const [title, setTitle] = useState(props.title);
-
-    const clickedHandler = () => {
-        setTitle('Updated!');
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={ props.date }/>
 
             <div className="expense-item__description">
-                <h2>{ title }</h2>
+                <h2>{ props.title }</h2>
 
                 <div className="expense-item__price">${ props.amount }</div>
             </div>
-
-            <button
-                onClick={ clickedHandler }
-            >
-                Change Title
-            </button>
         </Card>
     );
 }
--- basics/src/components/Expenses/Expenses.js
+++ basics/src/components/Expenses/Expenses.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // CSS
 import './Expenses.css';
 
 // JSX
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses( props ) {
+    const [state, setState] = useState({
+        filteredYear: '2020'
+    });
+
+    const filteredChangeHandler = selectedYear => {
+        setState( (prevState) => {
+            return {
+                ...prevState,
+                filteredYear: selectedYear
+            };
+        });
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter 
+                onChangeFilter={ filteredChangeHandler }
+                selected={ state.filteredYear }
+            />
+
             <ExpenseItem
                 amount={ props.items[0].amount }
                 date={ props.items[0].date }
