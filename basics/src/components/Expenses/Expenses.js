43f8e32c9283feb1911bd1381ad2d4fc8c4f09cb
michael rondon <mrondon72@gmail.com>
2021-04-21 19:47:24 +0200

    63. Rendering Lists of Data

--- basics/src/components/Expenses/Expenses.js
+++ basics/src/components/Expenses/Expenses.js
@@ -29,29 +29,13 @@ function Expenses( props ) {
                 selected={ state.filteredYear }
             />
 
-            <ExpenseItem
-                amount={ props.items[0].amount }
-                date={ props.items[0].date }
-                title={ props.items[0].title }
-            />
-
-            <ExpenseItem
-                amount={ props.items[1].amount }
-                date={ props.items[1].date }
-                title={ props.items[1].title }
-            />
-
-            <ExpenseItem
-                amount={ props.items[2].amount }
-                date={ props.items[2].date }
-                title={ props.items[2].title }
-            />
-
-            <ExpenseItem
-                amount={ props.items[3].amount }
-                date={ props.items[3].date }
-                title={ props.items[3].title }
-            />
+            {props.item.map( expense => (
+                <Expense 
+                    amount={ expense.amount }
+                    date={ expense.date }
+                    title={ expense.title }
+                />
+            ))}
         </Card>
     );
 }
