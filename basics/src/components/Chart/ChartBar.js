f3ebb3599e3e6b37ae15cc892b3129ae7a80abb9
mrondon <mrondon-externo@cnmc.es>
2021-04-28 16:19:50 +0200

    69. Adding Dynamic Styles

---
+++ basics/src/components/Chart/ChartBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// CSS
+import './ChartBar.css';
+
+const ChartBar = props => {
+    let barFillHight = '0%';
+
+    if ( props.max > 0 ) {
+        barFillHight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div 
+                    className="chart-bar__fill"
+                    style={{height: barFillHight}}
+                ></div>
+            </div>
+
+            <div className="chart-bar__label">
+                { props.label }
+            </div>
+        </div>
+    );
+};
+
+export default ChartBar;
